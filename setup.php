22e63390bbd235c79e8fd21da2f3bbd6968efc3f
Canyuan Tian <ctian3@hawk.iit.edu>
2015-12-01 12:59:05 -0600

    fix the problem that could not load database

--- setup.php
+++ setup.php
@@ -18,43 +18,26 @@ $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 print "=====================\n". $endpoint . "============\n"; 
 
 
-#foreach ($result->getPath('DBInstances/*/Endpoint/Address') as $ep) {
-#    // Do something with the message
-#    echo "============". $ep . "================";
-#    $endpoint = $ep;
-#}
-
-
 
 echo "begin database";
 $link = mysqli_connect($endpoint,"controller","letmein88","ctiandb") or die("Error " . mysqli_error($link));
 
-/* check connection */
+
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-/*
-$delete_table = 'DELETE TABLE student';
-$del_tbl = $link->query($delete_table);
-if ($delete_table) {
-        echo "Table student has been deleted";
-}
-else {
-        echo "error!!";
 
-}
-*/
 $create_table = 'CREATE TABLE IF NOT EXISTS items  
 (
-    uname VARCHAR (30) NOT NULL,
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(200) NOT NULL,
     phone VARCHAR(20) NOT NULL,
-    filename VARCHAR(255) NOT NULL,
     s3rawurl VARCHAR(255) NOT NULL,
+    filename VARCHAR(255) NOT NULL,
     s3finishedurl VARCHAR(255) NOT NULL,
-    status INT NOT NULL
+    status INT NOT NULL,
+    issubscribed INT,
 )';
 
 
